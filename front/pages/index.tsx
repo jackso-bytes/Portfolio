86ae6d51de8d5070c50d1d52469280f4e15fdb13
jackso-bytes <81561260+jackso-bytes@users.noreply.github.com>
2022-09-11 18:28:06 +0100

    add portfolio item

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,10 +1,17 @@
 import type { NextPage } from "next";
 import Layout from "../layouts/Layout";
+import PortfolioItem from "../components/Portfolioite";
+
+const fetchParams = {};
 
 const Home: NextPage = () => {
   return (
     <Layout>
-      <h1 className="text-3xl font-bold ">Hello world</h1>
+      <section className="bg-white dark:bg-gray-900">
+        <div className="container px-6 py-10 mx-auto">
+          <PortfolioItem />
+        </div>
+      </section>
     </Layout>
   );
 };
