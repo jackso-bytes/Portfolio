a834ed93e73e3913ca4030a0209267c6bea20f62
jackso-bytes <81561260+jackso-bytes@users.noreply.github.com>
2022-09-15 20:15:56 +0100

    add IconLink comp

---
+++ front/components/IconLink.tsx
@@ -0,0 +1,17 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+
+export interface IconLinkProps {
+  link: string;
+  icon: IconDefinition;
+  text: string;
+}
+
+export const IconLink = ({ link, icon, text }: IconLinkProps) => {
+  return (
+    <a href={link} className="flex items-center pt-2">
+      <FontAwesomeIcon icon={icon} className="pr-4" />
+      <p>{text}</p>
+    </a>
+  );
+};
