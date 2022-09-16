00298f3ddc021e76ce21c5dff4d568004794eff5
jackso-bytes <81561260+jackso-bytes@users.noreply.github.com>
2022-09-16 18:14:21 +0100

    rm unused nav structure

--- back/src/api/nav-structure/controllers/nav-structure.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * nav-structure controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::nav-structure.nav-structure');
--- back/src/api/nav-structure/services/nav-structure.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * nav-structure service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::nav-structure.nav-structure');
