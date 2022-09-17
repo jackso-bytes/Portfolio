e7b77e36b3d4b94eb134c9c902f68d5cd530f8c0
jackso-bytes <81561260+jackso-bytes@users.noreply.github.com>
2022-09-17 09:17:16 +0100

    add about page

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from "next";
 import { GetStaticProps } from "next";
 import Layout from "../layouts/Layout";
 import { IconLink } from "../components/IconLink";
-import { PortfolioItemsResType, PortfolioItemType } from "../types";
 import {
   faGithub,
   faYoutube,
@@ -20,43 +19,33 @@ const fetchParams = {
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const res = await fetch(`${URL}/api/portfolio-items`, fetchParams),
-    portfolioItems: PortfolioItemsResType = await res.json();
+  const res = await fetch(`${URL}/api/about-contents`, fetchParams),
+    aboutContent = await res.json();
+  console.log(aboutContent);
   return {
-    props: portfolioItems,
+    props: aboutContent,
   };
 };
 
-const Home: NextPage<{ data: PortfolioItemType[] }> = ({ data }) => {
+const Home: NextPage<{ data: any }> = ({ data }) => {
+  const { title, body, gitHubShowCase, youTubeShowCase } = data[0].attributes;
   return (
     <Layout>
       <section className="bg-white dark:bg-gray-900">
-        <div className="container py-5 mx-auto space-y-4">
+        <div className="container py-5 mx-auto space-y-8 mt-8">
           <img
             className="object-cover rounded-full w-52 h-52 ring-4 ring-gray-300 dark:ring-gray-700 m-auto"
             src="https://images.unsplash.com/photo-1488508872907-592763824245?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
             alt=""
           ></img>
           <h1 className="text-5xl font-bold text-gray-800 dark:text-white text-center">
-            Hi, I'm Tom
+            {title}
           </h1>
-          <p className="text-gray-500 dark:text-gray-400 text-center">
-            Hi I'm Tom a web developer with a passion for all things JavaScript
-            related. I have a fair bit of experience with the usual front-end
-            tools like React and I'm constantly looking to improve on that
-            knowledge base. For example, I used Next to build this portfolio and
-            set it up to consume a Strapi back-end. I love solving real world
-            problems with code. In my spare time I make tutorials and interview
-            cool people who inspire me.
-          </p>
+          <p className="text-gray-500 dark:text-gray-400 text-center">{body}</p>
           <div className="flex justify-center space-x-8">
+            <IconLink link={gitHubShowCase} icon={faGithub} text="GitHub" />
             <IconLink
-              link="https://github.com/jackso-bytes"
-              icon={faGithub}
-              text="GitHub"
-            />
-            <IconLink
-              link="https://www.youtube.com/watch?v=9l3r0EFp9ow&t=1235s"
+              link={youTubeShowCase}
               icon={faYoutube}
               iconColorClass="text-red-500"
               text="YouTube"
