b39f8383cd4bf7c25b890058dbc74c3a64e05e70
jackso-bytes <81561260+jackso-bytes@users.noreply.github.com>
2022-09-20 17:50:30 +0100

    bring img into index

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -19,7 +19,7 @@ const fetchParams = {
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const res = await fetch(`${URL}/api/about-contents`, fetchParams),
+  const res = await fetch(`${URL}/api/about-contents?populate=*`, fetchParams),
     aboutContent = await res.json();
   return {
     props: aboutContent,
@@ -27,16 +27,20 @@ export const getStaticProps: GetStaticProps = async (context) => {
 };
 
 const Home: NextPage<{ data: any }> = ({ data }) => {
-  const { title, body, gitHubShowCase, youTubeShowCase } = data[0].attributes;
+  const { title, body, gitHubShowCase, youTubeShowCase, heroImage } =
+    data[0].attributes;
+
   return (
     <Layout>
       <section className="bg-white dark:bg-gray-900">
-        <div className="container flex flex-col align-center justify-center py-5 mx-auto space-y-8 mt-8">
-          <img
-            className="object-cover rounded-full w-52 h-52 ring-4 ring-gray-300 dark:ring-gray-700 m-auto"
-            src="https://images.unsplash.com/photo-1488508872907-592763824245?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-            alt=""
-          ></img>
+        <div className="container flex flex-col items-center justify-center py-5 mx-auto space-y-8 mt-8">
+          <div className="w-14">
+            <img
+              className="object-cover rounded-full w-52 h-52 ring-4 ring-gray-300 dark:ring-gray-700 m-auto"
+              src={heroImage.data[0].attributes.url}
+              alt="hero-image"
+            ></img>
+          </div>
           <h1 className="text-5xl font-bold text-gray-800 dark:text-white text-center">
             {title}
           </h1>
