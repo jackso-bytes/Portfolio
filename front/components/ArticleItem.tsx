415a450809e66676a7c31e77dc698e85c21eb422
jackso-bytes <81561260+jackso-bytes@users.noreply.github.com>
2022-09-21 12:15:10 +0100

    add img to articles

--- front/components/ArticleItem.tsx
+++ front/components/ArticleItem.tsx
@@ -8,10 +8,10 @@ const ArticleItem = ({ attributes }: ArticleItemType) => {
       <a href={url}>
         <img
           className="relative z-10 object-cover w-full rounded-md h-96"
-          src="https://images.unsplash.com/photo-1644018335954-ab54c83e007f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+          src={attributes.image.data.attributes.url}
           alt=""
         ></img>
-        <div className="relative z-20 max-w-lg p-6 mx-auto -mt-20 bg-white rounded-md shadow dark:bg-gray-900">
+        <div className="relative z-20 p-6 mx-auto -mt-20 bg-white rounded-md shadow dark:bg-gray-900">
           <h2 className="font-semibold text-gray-800 dark:text-white md:text-xl">
             {title}
           </h2>
