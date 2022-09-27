185f2365c8c38baecab992cbad0536952bdf41cd
jackso-bytes <81561260+jackso-bytes@users.noreply.github.com>
2022-09-27 09:15:46 +0100

    increase vertical spacing

--- front/layouts/Layout.tsx
+++ front/layouts/Layout.tsx
@@ -5,7 +5,7 @@ const Layout = ({ children }: React.PropsWithChildren) => {
   return (
     <div>
       <Header />
-      <main className="container px-6 py-4 mx-auto">{children}</main>
+      <main className="container px-6 py-8 mx-auto">{children}</main>
       <Footer />
     </div>
   );
